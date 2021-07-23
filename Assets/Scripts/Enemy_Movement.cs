ca545164e45e36feead788974a16943d8f94e0a9
SLED\Iani <marzia.faustinelli@gmail.com>
2021-07-24 00:59:49 +0200

    enemy follows given path

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Enemy_Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] List<Tile_Waypoint> path;
+    [Tooltip("Tiles per second")] [SerializeField] [Range(0f, 5f)] float speed = 1f;
+
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        StartCoroutine(FollowPath());
     }
+
+    private IEnumerator FollowPath()
+    {
+        foreach (Tile_Waypoint waypoint in path)
+        {
+            Vector3 startingPosition = transform.position;
+            Vector3 targetPosition = waypoint.transform.position;
+
+            float lerpPhase = 0f;
+
+            while (lerpPhase < 1)
+            {
+                lerpPhase += speed * Time.deltaTime;
+                transform.position = Vector3.Lerp(startingPosition, targetPosition, lerpPhase);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
 }
