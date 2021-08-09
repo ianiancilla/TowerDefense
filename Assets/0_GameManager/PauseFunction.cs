12e2993853100c41333a89bcfa21387c45d6d3b2
SLED\Iani <marzia.faustinelli@gmail.com>
2021-08-09 23:22:46 +0200

    folder tidying, pause function, set particles to unscaled time

---
+++ Assets/0_GameManager/PauseFunction.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseFunction : MonoBehaviour
+{
+    // properties
+    [SerializeField] KeyCode pause_UnpauseButton;
+    
+    // variables
+    bool isPaused = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(pause_UnpauseButton))
+        {
+            GamePause();
+        }
+    }
+
+
+    private void GamePause()
+    {
+        if (isPaused) { Time.timeScale = 1; }    // unpause if paused
+        else { Time.timeScale = 0; }    // pause if running
+        
+        isPaused = !isPaused;
+    }
+
+}
